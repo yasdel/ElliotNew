0c8fcf36f8c91a14526d4523dd0d8602af44a901
yasdel <yashar.deldjoo@gmail.com>
2021-05-08 15:04:20 +0200

    commit and push

--- read_pandas_csv.py
+++ read_pandas_csv.py
@@ -5,9 +5,16 @@ addr = '/home/yas/PycharmProjects/elliotNew/data/'
 read_ds =  addr + 'AmazonDigMusic/ratings_Digital_Music.csv'
 write_ds =  addr+ 'AmazonDigMusic/ratings_Digital_Music_deli_tab.csv'
 
-csv_data = pd.read_csv(read_ds)
-print(csv_data)
+#read_ds =  addr + 'Epinions/ratings_data.txt'
+#write_ds =  addr+ 'Epinions/ratings_data_deli_tab.csv'
 
-csv_data.to_csv(write_ds, sep = '\t')
+csv_data = pd.read_csv(read_ds)
+print(csv_data.head())
+
+print(csv_data.shape)
+
+csv_data.to_csv(write_ds, index= False, sep = '\t')
+
+
 
 
