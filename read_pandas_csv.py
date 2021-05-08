997081d103a56271e712563483f25294ddb9efcc
yasdel <yashar.deldjoo@gmail.com>
2021-05-09 00:10:46 +0200

    commit and push

--- read_pandas_csv.py
+++ read_pandas_csv.py
@@ -8,12 +8,20 @@ write_ds =  addr+ 'AmazonDigMusic/ratings_Digital_Music_deli_tab.csv'
 read_ds =  addr + 'Epinions/ratings_data.txt'
 write_ds =  addr+ 'Epinions/ratings_data_deli_tab.csv'
 
-csv_data = pd.read_csv(read_ds, sep=' ')
+read_ds = addr + 'Foursq/dataset_ubicomp2013_checkins.txt'
+write_ds =  addr+ 'Foursq/dataset_ubicomp2013_checkins_deli_tab.csv'
+
+#csv_data = pd.read_csv(read_ds, sep=' ')
+csv_data = pd.read_csv(read_ds,sep = '\t')
 print(csv_data.head())
 
 print(csv_data.shape)
 
-csv_data.to_csv(write_ds, index= False, sep = '\t')
+csv_data.columns = ['UserId','ItemId']
+csv_data['new'] = csv_data.apply(lambda x: 1, axis=1)
+print(csv_data)
+
+csv_data.to_csv(write_ds, index= False, sep = '\t', header=False)
 
 
 
