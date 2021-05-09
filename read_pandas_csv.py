18790909dc11274572898ee2dfe7350becc390bc
yasdel <yashar.deldjoo@gmail.com>
2021-05-09 08:24:26 +0200

    commit and push

--- read_pandas_csv.py
+++ read_pandas_csv.py
@@ -11,15 +11,19 @@ write_ds =  addr+ 'Epinions/ratings_data_deli_tab.csv'
 read_ds = addr + 'Foursq/dataset_ubicomp2013_checkins.txt'
 write_ds =  addr+ 'Foursq/dataset_ubicomp2013_checkins_deli_tab.csv'
 
+read_ds = addr + 'AmazonOffice/ratings_Office_Products.csv'
+write_ds = addr + 'AmazonOffice/ratings_Office_Products_del_tab.csv'
+
+
 #csv_data = pd.read_csv(read_ds, sep=' ')
-csv_data = pd.read_csv(read_ds,sep = '\t')
+csv_data = pd.read_csv(read_ds,sep = ',')
 print(csv_data.head())
 
 print(csv_data.shape)
 
-csv_data.columns = ['UserId','ItemId']
-csv_data['new'] = csv_data.apply(lambda x: 1, axis=1)
-print(csv_data)
+#csv_data.columns = ['UserId','ItemId']
+#csv_data['new'] = csv_data.apply(lambda x: 1, axis=1)
+#print(csv_data)
 
 csv_data.to_csv(write_ds, index= False, sep = '\t', header=False)
 
